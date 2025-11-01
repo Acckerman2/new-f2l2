373659196179723c00ebdf6db0deb2e3256ac95b
Rahul R <rahulrsr2006@gmail.com>
2025-11-01 14:40:12 +0530

    Refactor comments and media handling in LoadStart
    Updated comments for clarity and added steps for sending media.

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -8,7 +8,7 @@ import (
 	"github.com/celestix/gotgproto/dispatcher/handlers"
 	"github.com/celestix/gotgproto/ext"
 	"github.com/celestix/gotgproto/storage"
-	"github.com/gotd/td/tg" // <-- This import is required
+	"github.com/gotd/td/tg" // This import is still required
 )
 
 // LoadStart registers the /start command handler
@@ -34,19 +34,29 @@ func start(ctx *ext.Context, u *ext.Update) error {
 		return dispatcher.EndGroups
 	}
 
-	// --- Send image with caption (This is the correct way) ---
+	// --- Send image with caption (This is the correct way for v1.0.0-beta18) ---
 	caption := "Hi, send me any file to get a direct streamble link to that file."
 	photoUrl := "https://envs.sh/NEV.jpg" // The URL you provided
 
-	// Use ctx.Reply (which works) and pass the media in ext.Other
-	_, err := ctx.Reply(u, caption, &ext.Other{
-		Media: &tg.InputMediaPhotoExternal{
-			URL: photoUrl,
-		},
-	})
+	// 1. Create the media object for the photo URL
+	media := &tg.InputMediaPhotoExternal{
+		URL: photoUrl,
+	}
 
-	// Just return the error. Do not use ctx.Client.Log
+	// 2. Create the SendMessage parameters object.
+	// The ctx.Reply function expects this as its *third* argument.
+	params := &tg.MessagesSendMessage{
+		Media: media,
+	}
+
+	// 3. Call ctx.Reply.
+	// The 2nd argument (caption) will be used as the message.
+	// The 3rd argument (params) provides the media.
+	_, err := ctx.Reply(u, caption, params)
+
+	// 4. Handle errors
 	if err != nil {
+		// Just return the error. Do not use ctx.Client.Log
 		return err
 	}
 
