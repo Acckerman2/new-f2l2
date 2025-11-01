8ed7ec11b25b04ef29dc9bcb28fb0fd7fb1ac1cb
Rahul R <rahulrsr2006@gmail.com>
2025-11-01 18:55:29 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,8 +1,5 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
-
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton  # <-- ADDED IMPORTS
 
 from WebStreamer.vars import Var 
 from WebStreamer.bot import StreamBot
@@ -15,6 +12,20 @@ async def start(_, m: Message):
             Check <a href='https://github.com/EverythingSuckz/TG-FileStreamBot#optional-vars'>this link</a> for more info.",
             disable_web_page_preview=True, quote=True
         )
-    await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+    
+    await m.reply_photo(
+        photo="https://envs.sh/NEV.jpg",
+        caption="✨ ʜɪ ɪ'ᴍ Sydney Sweeney! 📁🔗\n\n"
+                "🚀 ᴜᴘʟᴏᴀᴅ ᴀɴʏ ꜰɪʟᴇ ᴀɴᴅ ɢᴇᴛ ɪɴꜱᴛᴀɴᴛ ᴅɪʀᴇᴄᴛ ʟɪɴᴋꜱ 🌐\n\n"
+                "💎 ꜰᴀꜱᴛ ⚡ | ꜱᴇᴄᴜʀᴇ 🔒 | ᴇᴀꜱʏ ᴛᴏ ᴜꜱᴇ 💫\n\n"
+                "💬 ᴊᴜꜱᴛ ꜱᴇɴᴅ ᴀ ᴘʜᴏᴛᴏ, ᴠɪᴅᴇᴏ, ᴏʀ ᴅᴏᴄ — ᴀɴᴅ ɪ'ʟʟ ʜᴀɴᴅʟᴇ ᴛʜᴇ ʀᴇꜱᴛ 😎",
+        
+        # --- THIS IS THE NEW PART ---
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("👑 Owner", url="https.t.me/Acckerman_r2")]
+                # You can change the URL to your own Telegram link
+            ]
+        )
+        # ----------------------------
     )
